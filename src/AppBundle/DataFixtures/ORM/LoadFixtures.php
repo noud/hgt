770b56b4e149d722bd3a13999f15a9c70209f5ea
Dennis van den Hout <dennis@webdesigntilburg.nl>
2018-02-09 17:07:35 +0000

    tussentijds opslaan

--- src/AppBundle/DataFixtures/ORM/LoadFixtures.php
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -8,6 +8,33 @@ use Nelmio\Alice\Fixtures;
 
 class LoadFixtures implements FixtureInterface
 {
+    public function lowercase($string)
+    {
+        return strtolower($string);
+    }
+
+    public function fixTitle($string)
+    {
+        $separator = '-';
+        $quoteSeparator = preg_quote($separator, '#');
+
+        $trans = array(
+            '&.+?;'                    => '',
+            '[^\w\d _-]'            => '',
+            '\s+'                    => $separator,
+            '('.$quoteSeparator.')+'=> $separator
+        );
+
+        $string = strip_tags($string);
+        foreach ($trans as $key => $val){
+            $string = preg_replace('#'.$key.'#i', $val, $string);
+        }
+
+        $string = strtolower($string);
+
+        return trim(trim($string, $separator));
+    }
+
     public function load(ObjectManager $manager)
     {
         Fixtures::load(
