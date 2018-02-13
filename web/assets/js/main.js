3aeeb335f70c6dbc746a80fa2d5efa0ea2676b72
root <hieu.nguyen@webdesigntilburg.nl>
2018-02-13 10:19:17 +0000

    homepage in general

--- web/assets/js/main.js
+++ web/assets/js/main.js
@@ -13374,24 +13374,17 @@
 
 }));
 
+function executeAsync(functionArr, timeout) {
+  for (var i = 0; i < functionArr.length; i++) {
+    setTimeout(functionArr[i], timeout);
+  }
+}
+
 $(document).ready(function () {
 
+  'use strict';
+
   var $body = $('body');
-
-  var initShoppingCartLabel = function () {
-    if ($body.find('*').hasClass('js-shopping-cart')) {
-
-      var $shoppingCart = $('.shopping-cart');
-
-      var $shoppingCartIconMargin = $shoppingCart.find('.icon').css('margin-right');
-
-      var $shoppingCartIconWidth = $shoppingCart.find('.icon > span').outerWidth();
-
-      $shoppingCart.find('.icon').css({
-        'margin-right': $shoppingCartIconWidth + 2
-      });
-    }
-  };
 
 
   $('.primary-menu-trigger, .overlay').click(function () {
@@ -13400,10 +13393,29 @@ $(document).ready(function () {
   });
 
 
-  initShoppingCartLabel();
+  var initShoppingCartLabel = function () {
 
+    if ($body.find('*').hasClass('js-shopping-cart')) {
 
+      var $shoppingCart = $('.shopping-cart');
+      var $shoppingCartIconWidth = $shoppingCart.find('.icon > span').outerWidth();
+      $shoppingCart.find('.icon').css({
+        'margin-right': $shoppingCartIconWidth + 2
+      });
+    }
+  };
 
+  var initSlick = function () {
+    if ($body.find('*').hasClass('js-hero')) {
+      $('.hero').slick({
+        'arrows': true,
+        'prevArrow': '<a role="button" class="slick-prev"><i class="fas fa-chevron-left"></i></a>',
+        'nextArrow': '<a role="button" class="slick-next"><i class="fas fa-chevron-right"></i></a>'
+      });
+    }
+  };
+
+  executeAsync([initSlick, initShoppingCartLabel], 50);
 
 });
 
