e06d3c6545f29e75c794ee88d18bc0c079cb022b
root <hieu.nguyen@webdesigntilburg.nl>
2018-02-15 11:19:36 +0000

    added contact form, updates in general

--- src/AppBundle/Controller/Folder/FolderController.php
+++ src/AppBundle/Controller/Folder/FolderController.php
@@ -9,7 +9,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class IndexController extends Controller
+class FolderController extends Controller
 {
     /**
      * IndexController constructor.
---
+++ src/Application/Contact/Command/ContactCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace HGT\Application\Contact\Command;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ContactCommand
+{
+    /**
+     * @Assert\NotBlank()
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @Assert\NotBlank()
+     * @var string
+     */
+    public $phone;
+
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     * @var string
+     */
+    public $email;
+
+
+    /**
+     * @Assert\NotBlank()
+     * @var string
+     */
+    public $message;
+
+}
