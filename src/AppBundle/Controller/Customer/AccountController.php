6a4d1a8c5ab4baff4d56ddb4423dda14576efd2b
root <hieu.nguyen@webdesigntilburg.nl>
2018-03-16 08:30:58 +0000

    added index view, using for html and css structuring for other views

--- src/AppBundle/Controller/Customer/AccountController.php
+++ src/AppBundle/Controller/Customer/AccountController.php
@@ -4,10 +4,21 @@ namespace HGT\AppBundle\Controller\Customer;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class AccountController extends Controller
 {
     /**
+     * @Route("/mijn-account", name="account_index")
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render('account/order-history.html.twig', [
+        ]);
+    }
+
+    /**
+     * @throws \Exception
      * @Route("/logout", name="logout")
      */
     public function logoutAction()
