7a0705368f94c68de2dffab92eafcaaec085ba9f
root <hieu.nguyen@webdesigntilburg.nl>
2018-03-23 13:18:55 +0000

    initial start

--- src/AppBundle/Controller/Action/ActionController.php
+++ src/AppBundle/Controller/Action/ActionController.php
@@ -2,17 +2,20 @@
 
 namespace HGT\AppBundle\Controller\Action;
 
+use HGT\Application\Catalog\ProductPriceService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
-class IndexController extends Controller
+class ActionController extends Controller
 {
     /**
      * @Route("/acties", name="action_index")
      */
-    public function indexAction(Request $request)
-    {
-        return $this->render('actions/index.html.twig');
+    public function indexAction(
+        Request $request,
+        ProductPriceService $productPriceService
+    ) {
+        return $this->render('actions/index.html.twig', []);
     }
 }
